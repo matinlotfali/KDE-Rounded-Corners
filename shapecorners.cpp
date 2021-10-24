d2eaac5a2ba7be9a0cd25b290c0a0e4635292ab0
Matin Lotfaliei <matinlotfali@gmail.com>
2021-10-24 11:58:58 -0700

    Fix the wrong commit line

--- shapecorners.cpp
+++ shapecorners.cpp
@@ -215,7 +215,6 @@ ShapeCornersEffect::prePaintWindow(KWin::EffectWindow *w, KWin::WindowPrePaintDa
             || !w->isPaintingEnabled()
 //            || KWin::effects->hasActiveFullScreenEffect()
             || w->isDesktop()
-            || isMaximized(w)
             || (w == applyEffect)
 #if KWIN_EFFECT_API_VERSION < 233
            || data.quads.isTransformed()
@@ -263,7 +262,6 @@ ShapeCornersEffect::paintWindow(KWin::EffectWindow *w, int mask, QRegion region,
             || !w->isPaintingEnabled()
 //            || KWin::effects->hasActiveFullScreenEffect()
             || w->isDesktop()
-            || isMaximized(w)
             || (w == applyEffect)
 #if KWIN_EFFECT_API_VERSION < 233
             || data.quads.isTransformed()
