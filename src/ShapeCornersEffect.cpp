d3babe5b22d0368f7c67f17a4bdeec6e8d29615f
Matin Lotfaliei <matinlotfali@gmail.com>
2024-02-01 19:03:48 -0800

    Add dialogs to the included windows

--- src/ShapeCornersEffect.cpp
+++ src/ShapeCornersEffect.cpp
@@ -170,7 +170,7 @@ bool ShapeCornersEffect::hasEffect(const KWin::EffectWindow *w) const {
     const auto name = w->windowClass().split(QChar::Space).first();
     return m_shaderManager.IsValid()
            && m_managed.contains(w)
-           && (w->isNormalWindow() || ShapeCornersConfig::inclusions().contains(name))
+           && (w->isNormalWindow() || w->isDialog() || ShapeCornersConfig::inclusions().contains(name))
            && !ShapeCornersConfig::exclusions().contains(name);
 }
 
