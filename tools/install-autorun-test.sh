021b7012e51bd8580dd4b0a123030b78609083a9
Matin Lotfaliei <matinlotfali@gmail.com>
2024-03-15 22:54:56 -0700

    Update and rename autorun-test-install.sh to install-autorun-test.sh

--- tools/install-autorun-test.sh
+++ tools/install-autorun-test.sh
@@ -3,4 +3,4 @@ GIT_PATH="$(dirname "$(pwd)")"
 mkdir -p ~/.config/autostart
 cp "${GIT_PATH}"/tools/test-install.desktop ${AUTOSTART_FILE}
 sed -i -e "s|Exec=|Exec=sh ${GIT_PATH}/|g" ${AUTOSTART_FILE}
-sed -i -e "s|Path=|Path=${GIT_PATH}|g" ${AUTOSTART_FILE}
+sed -i -e "s|Path=|Path=${GIT_PATH}/|g" ${AUTOSTART_FILE}
