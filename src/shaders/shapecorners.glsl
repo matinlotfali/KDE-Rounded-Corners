bab04a585c8a3badccafc04f4e095834ff0ce0e7
Matin Lotfaliei <matinlotfali@gmail.com>
2024-05-21 00:37:02 -0700

    Add transparency condition for outline rendering

--- src/shaders/shapecorners.glsl
+++ src/shaders/shapecorners.glsl
@@ -74,7 +74,7 @@ vec4 shapeCorner(vec2 coord0, vec4 tex, vec2 start, float angle) {
         c = getShadowColor(distance_from_center);
     }
 
-    if(isDrawingOutline()) {
+    if(tex.a > 0.0 && isDrawingOutline()) {
         vec4 outlineOverlay = vec4(mix(tex.rgb, outlineColor.rgb, outlineColor.a), 1.0);
 
         if (distance_from_center < r - outlineThickness + 0.5) {
