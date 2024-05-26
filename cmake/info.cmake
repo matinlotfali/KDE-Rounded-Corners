6db25870ea4fc8b8badf33e5f39134da29669ac0
Matin Lotfaliei <matinlotfali@gmail.com>
2024-05-26 13:51:18 -0700

    Add plasma version in cmake

--- cmake/info.cmake
+++ cmake/info.cmake
@@ -1,12 +1,14 @@
+message(" * KWin Version: ${KWin_VERSION}")
+
 if(${QT_MAJOR_VERSION} EQUAL 6)
     set(KWIN_EFFECT_INCLUDE_FILE "/usr/include/kwin/effect/effect.h")
 else ()
     set(KWIN_EFFECT_INCLUDE_FILE "/usr/include/kwineffects.h")
 endif ()
-
 execute_process(
         COMMAND sh -c "grep '#define KWIN_EFFECT_API_VERSION_MINOR' ${KWIN_EFFECT_INCLUDE_FILE} | awk '{print \$NF}'"
         OUTPUT_VARIABLE KWIN_EFFECT_API_VERSION_MINOR OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-message("KWinEffect API Version: ${KWIN_EFFECT_API_VERSION_MINOR}")
+message(" * KWinEffect Version: ${KWIN_EFFECT_API_VERSION_MINOR}")
+
 message("")
