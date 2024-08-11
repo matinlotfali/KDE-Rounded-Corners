17440bb89e767addfaaed5d35b5a225448d4eba9
Matin Lotfaliei <matinlotfali@gmail.com>
2024-08-10 17:52:11 -0700

    Change default to Qt6 and KF6

--- cmake/qtversion.cmake
+++ cmake/qtversion.cmake
@@ -1,7 +1,7 @@
 find_package(KF6 QUIET COMPONENTS ConfigWidgets)
 
-if(KF6_FOUND)
-    set(QT_MIN_VERSION "6.4.0")
-    set(QT_MAJOR_VERSION 6)
-    set(KF_MIN_VERSION 6)
-endif (KF6_FOUND)
+if(${KF6_FOUND} EQUAL 0)
+    set(QT_MIN_VERSION "5.15")
+    set(QT_MAJOR_VERSION 5)
+    set(KF_MIN_VERSION "5.78")
+endif ()
