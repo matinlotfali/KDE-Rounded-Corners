07dc764f9cb6c4d247aec0d953ca03b10bff37c6
Robert Devasia <robertdevasia@outlook.com>
2025-02-12 16:34:13 -0500

    `qdbus-qt6` has been changed to `qdbus6`

--- tools/load.sh
+++ tools/load.sh
@@ -6,4 +6,4 @@ dbus-send --dest=org.kde.KWin --print-reply /Effects \
   org.kde.kwin.Effects.loadEffect string:kwin4_effect_shapecorners | awk 'NR==2 {print $2}'
 
 kwriteconfig6 --file breezerc --group Common --key OutlineIntensity "OutlineOff"
-qdbus-qt6 org.kde.KWin /KWin reconfigure
+qdbus6 org.kde.KWin /KWin reconfigure
