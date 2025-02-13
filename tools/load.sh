558f5eb333a9398056747f4ea778134df79e81f3
Robert Devasia <robertdevasia@outlook.com>
2025-02-12 19:18:22 -0500

    `qdbus-qt6` has been changed to `qdbus6` (#322)

--- tools/load.sh
+++ tools/load.sh
@@ -6,4 +6,13 @@ dbus-send --dest=org.kde.KWin --print-reply /Effects \
   org.kde.kwin.Effects.loadEffect string:kwin4_effect_shapecorners | awk 'NR==2 {print $2}'
 
 kwriteconfig6 --file breezerc --group Common --key OutlineIntensity "OutlineOff"
-qdbus-qt6 org.kde.KWin /KWin reconfigure
+
+# Find available qdbus binary
+QDBUS_BIN=$(which qdbus6 qdbus-qt6 qdbus 2>/dev/null | head -n 1)
+
+if [ -z "$QDBUS_BIN" ]; then
+    echo "qdbus not found. Exiting."
+    exit 1
+fi
+
+$QDBUS_BIN org.kde.KWin /KWin reconfigure
