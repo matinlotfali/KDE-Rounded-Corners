bacf3a2c099ad6ee6d03794a3270c0d9a08ab9c7
金雄镕 <dragove@qq.com>
2025-05-09 13:06:32 +0800

    fix: fix unknown command qdbus-qt6 (#344)

--- tools/unload.sh
+++ tools/unload.sh
@@ -6,4 +6,12 @@ dbus-send --dest=org.kde.KWin --print-reply /Effects \
   org.kde.kwin.Effects.unloadEffect string:kwin4_effect_shapecorners > /dev/null
 
 kwriteconfig6 --file breezerc --group Common --key OutlineIntensity "OutlineMedium"
-qdbus-qt6 org.kde.KWin /KWin reconfigure
+
+# Find available qdbus binary
+QDBUS_BIN=$(which qdbus6 qdbus-qt6 qdbus 2>/dev/null | head -n 1)
+
+if [ -z "$QDBUS_BIN" ]; then
+    echo "qdbus not found. Exiting."
+    exit 1
+fi
+$QDBUS_BIN org.kde.KWin /KWin reconfigure
