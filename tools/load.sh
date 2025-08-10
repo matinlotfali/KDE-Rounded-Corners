7010f87515aa70e66cabd0a96c88bc98f5495a23
Matin Lotfaliei <matinlotfali@gmail.com>
2025-08-10 16:48:19 -0700

    Automatically disables/restores Breeze outline (#401)
    * Automatically disables/restores Breeze outline
    
    The Breeze window decoration's outline can cause visual glitches.
    
    This change automatically disables the outline when the effect is loaded
    and restores the previous setting when the effect is unloaded.
    This avoids manual configuration by the user.
    
    * Removes redundant include
    
    Removes the duplicate include of the QDBusMessage header file.
    
    The correct header is now used.

--- tools/load.sh
+++ tools/load.sh
@@ -5,8 +5,6 @@ set -e
 dbus-send --dest=org.kde.KWin --print-reply /Effects \
   org.kde.kwin.Effects.loadEffect string:kwin4_effect_shapecorners | awk 'NR==2 {print $2}'
 
-kwriteconfig6 --file breezerc --group Common --key OutlineIntensity "OutlineOff"
-
 # Find available qdbus binary
 QDBUS_BIN=$(which qdbus6 qdbus-qt6 qdbus 2>/dev/null | head -n 1)
 
