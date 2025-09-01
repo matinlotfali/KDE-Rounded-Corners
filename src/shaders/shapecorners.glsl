91f53b88e7091f5f9852e6243d5cc8b89eb75615
Matin Lotfaliei <matinlotfali@gmail.com>
2025-09-01 14:47:38 -0700

    Refactor and validate shaders with glslangValidator
    Streamlines shader validation using glslangValidator in a GitHub Actions workflow.
    
    Moves common shader variables into a shared include file, simplifying shader code and promoting reusability.
    
    Adds VS Code configuration for improved development experience, including recommended extensions and settings for C++, CMake, and glslangValidator.
    
    Also includes a task to download necessary GLSL shader files for linting.

--- src/shaders/shapecorners.glsl
+++ src/shaders/shapecorners.glsl
@@ -1,28 +1,3 @@
-uniform float radius;            // The thickness of the outline in pixels specified in settings.
-uniform vec2 windowSize;         // Containing `window->frameGeometry().size()`
-uniform vec2 windowExpandedSize; // Containing `window->expandedGeometry().size()`
-
-uniform vec2 windowTopLeft;      /* The distance between the top-left of `expandedGeometry` and
-                                  * the top-left of `frameGeometry`. When `windowTopLeft = {0,0}`, it means
-                                  * `expandedGeometry = frameGeometry` and there is no shadow. */
-
-uniform vec4 outlineColor;       // The RGBA of the outline color specified in settings.
-uniform float outlineThickness;  // The thickness of the outline in pixels specified in settings.
-uniform vec4 secondOutlineColor; // The RGBA of the second outline color specified in settings.
-uniform float secondOutlineThickness;  // The thickness of the second outline in pixels specified in settings.
-
-vec2 tex_to_pixel(vec2 texcoord) {
-    return vec2(texcoord0.x * windowExpandedSize.x - windowTopLeft.x,
-                (1.0-texcoord0.y)* windowExpandedSize.y - windowTopLeft.y);
-}
-vec2 pixel_to_tex(vec2 pixelcoord) {
-    return vec2((pixelcoord.x + windowTopLeft.x) / windowExpandedSize.x,
-                1.0-(pixelcoord.y + windowTopLeft.y) / windowExpandedSize.y);
-}
-bool hasExpandedSize() { return windowSize != windowExpandedSize; }
-bool hasPrimaryOutline() { return outlineColor.a > 0.0 && outlineThickness > 0.0; }
-bool hasSecondOutline() { return hasExpandedSize() && secondOutlineColor.a > 0.0 && secondOutlineThickness > 0.0; }
-
 #include "shapecorners_shadows.glsl"
 
 bool is_within(float point, float a, float b) {
@@ -113,7 +88,7 @@ vec4 shapeCorner(vec2 coord0, vec4 tex, vec2 start, float angle, vec4 coord_shad
     return mix(coord_shadowColor, tex, antialiasing);
 }
 
-vec4 run(vec4 tex) {
+vec4 run(vec2 texcoord0, vec4 tex) {
     if(tex.a == 0.0) {
         return tex;
     }
--- src/shaders/shapecorners_qt6_core.frag
+++ src/shaders/shapecorners_qt6_core.frag
@@ -1,6 +1,5 @@
 #version 140
 
-uniform sampler2D sampler;       // The painted contents of the window.
 in vec2 texcoord0;               // The XY location of the rendering pixel. Starting from {0.0, 0.0} to {1.0, 1.0}
 out vec4 fragColor;              // The RGBA color that can be rendered
 
@@ -18,7 +17,7 @@ void main(void)
 
     // to preserve perceptual contrast, apply the inversion in gamma 2.2 space
     tex = nitsToEncoding(tex, gamma22_EOTF, 0.0, destinationReferenceLuminance);
-    tex = run(tex);
+    tex = run(texcoord0, tex);
     tex *= modulation;
     tex.rgb *= tex.a;
     tex = encodingToNits(tex, gamma22_EOTF, 0.0, destinationReferenceLuminance);
